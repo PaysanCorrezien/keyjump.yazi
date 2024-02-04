958249cfc05d77c210b9fe734abdc970c5c1183f
DreamMaoMao <2523610504@qq.com>
2024-02-04 13:46:30 +0800

    optimize var name

--- init.lua
+++ init.lua
@@ -2,56 +2,63 @@ local M = {}
 
 local args = { ... }
 
-local t_signalKey_candy = {
-{on={'p'}}, {on={'b'}},
-{on={'e'}}, {on={'t'}},
-{on={'a'}}, {on={'o'}},
-{on={'i'}}, {on={'n'}},
-{on={'s'}}, {on={'r'}},
-{on={'h'}}, {on={'l'}},
-{on={'d'}}, {on={'c'}},
-{on={'u'}}, {on={'m'}},
-{on={'f'}}, {on={'g'}},
-{on={'w'}}, {on={'v'}},
-{on={'k'}}, {on={'j'}},
-{on={'x'}}, {on={'z'}},
-{on={'y'}}, {on={'q'}}
+local SINGLE_KEYS = {
+	"p", "b", "e", "t", "a", "o", "i", "n", "s", "r", "h", "l", "d", "c",
+	"u", "m", "f", "g", "w", "v", "k", "j", "x", "z", "y", "q"
+}
+-- stylua: ignore
+local DOUBLE_KEYS = {
+	"au", "ai", "ao", "ah", "aj", "ak", "al", "an", "su", "si", "so", "sh",
+	"sj", "sk", "sl", "sn", "du", "di", "do", "dh", "dj", "dk", "dl", "dn",
+	"fu", "fi", "fo", "fh", "fj", "fk", "fl", "fn", "gu", "gi", "go", "gh",
+	"gj", "gk", "gl", "gn", "eu", "ei", "eo", "eh", "ej", "ek", "el", "en",
+	"ru", "ri", "ro", "rh", "rj", "rk", "rl", "rn", "cu", "ci", "co", "ch",
+	"cj", "ck", "cl", "cn", "wu", "wi", "wo", "wh", "wj", "wk", "wl", "wn"
+}
+-- stylua: ignore
+local SIGNAL_CANDS = {
+	{ on = "p" }, { on = "b" }, { on = "e" }, { on = "t" }, { on = "a" },
+	{ on = "o" }, { on = "i" }, { on = "n" }, { on = "s" }, { on = "r" },
+	{ on = "h" }, { on = "l" }, { on = "d" }, { on = "c" }, { on = "u" },
+	{ on = "m" }, { on = "f" }, { on = "g" }, { on = "w" }, { on = "v" },
+	{ on = "k" }, { on = "j" }, { on = "x" }, { on = "z" }, { on = "y" },
+	{ on = "q" },
+}
+-- stylua: ignore
+local DOUBLE_CANDS = {
+	{ on = { "a", "u" } }, { on = { "a", "i" } }, { on = { "a", "o" } },
+	{ on = { "a", "h" } }, { on = { "a", "j" } }, { on = { "a", "k" } },
+	{ on = { "a", "l" } }, { on = { "a", "n" } }, { on = { "s", "u" } },
+	{ on = { "s", "i" } }, { on = { "s", "o" } }, { on = { "s", "h" } },
+	{ on = { "s", "j" } }, { on = { "s", "k" } }, { on = { "s", "l" } },
+	{ on = { "s", "n" } }, { on = { "d", "u" } }, { on = { "d", "i" } },
+	{ on = { "d", "o" } }, { on = { "d", "h" } }, { on = { "d", "j" } },
+	{ on = { "d", "k" } }, { on = { "d", "l" } }, { on = { "d", "n" } },
+	{ on = { "f", "u" } }, { on = { "f", "i" } }, { on = { "f", "o" } },
+	{ on = { "f", "h" } }, { on = { "f", "j" } }, { on = { "f", "k" } },
+	{ on = { "f", "l" } }, { on = { "f", "n" } }, { on = { "g", "u" } },
+	{ on = { "g", "i" } }, { on = { "g", "o" } }, { on = { "g", "h" } },
+	{ on = { "g", "j" } }, { on = { "g", "k" } }, { on = { "g", "l" } },
+	{ on = { "g", "n" } }, { on = { "e", "u" } }, { on = { "e", "i" } },
+	{ on = { "e", "o" } }, { on = { "e", "h" } }, { on = { "e", "j" } },
+	{ on = { "e", "k" } }, { on = { "e", "l" } }, { on = { "e", "n" } },
+	{ on = { "r", "u" } }, { on = { "r", "i" } }, { on = { "r", "o" } },
+	{ on = { "r", "h" } }, { on = { "r", "j" } }, { on = { "r", "k" } },
+	{ on = { "r", "l" } }, { on = { "r", "n" } }, { on = { "c", "u" } },
+	{ on = { "c", "i" } }, { on = { "c", "o" } }, { on = { "c", "h" } },
+	{ on = { "c", "j" } }, { on = { "c", "k" } }, { on = { "c", "l" } },
+	{ on = { "c", "n" } }, { on = { "w", "u" } }, { on = { "w", "i" } },
+	{ on = { "w", "o" } }, { on = { "w", "h" } }, { on = { "w", "j" } },
+	{ on = { "w", "k" } }, { on = { "w", "l" } }, { on = { "w", "n" } },
 }
 
-local t_doubleKey_candy = {
-    {on = {"a","u"}}, {on = {"a","i"}}, {on={"a","o"}}, {on={"a","h"}}, {on={"a","j"}}, {on={"a","k"}}, {on={"a","l"}}, {on={"a","n"}},
-    {on = {"s","u"}}, {on = {"s","i"}}, {on={"s","o"}}, {on={"s","h"}}, {on={"s","j"}}, {on={"s","k"}}, {on={"s","l"}}, {on={"s","n"}},
-    {on = {"d","u"}}, {on = {"d","i"}}, {on={"d","o"}}, {on={"d","h"}}, {on={"d","j"}}, {on={"d","k"}}, {on={"d","l"}}, {on={"d","n"}},
-    {on = {"f","u"}}, {on = {"f","i"}}, {on={"f","o"}}, {on={"f","h"}}, {on={"f","j"}}, {on={"f","k"}}, {on={"f","l"}}, {on={"f","n"}},
-	{on = {"g","u"}}, {on = {"g","i"}}, {on={"g","o"}}, {on={"g","h"}}, {on={"g","j"}}, {on={"g","k"}}, {on={"g","l"}}, {on={"g","n"}},
-    {on = {"e","u"}}, {on = {"e","i"}}, {on={"e","o"}}, {on={"e","h"}}, {on={"e","j"}}, {on={"e","k"}}, {on={"e","l"}}, {on={"e","n"}},
-    {on = {"r","u"}}, {on = {"r","i"}}, {on={"r","o"}}, {on={"r","h"}}, {on={"r","j"}}, {on={"r","k"}}, {on={"r","l"}}, {on={"r","n"}},
-    {on = {"c","u"}}, {on = {"c","i"}}, {on={"c","o"}}, {on={"c","h"}}, {on={"c","j"}}, {on={"c","k"}}, {on={"c","l"}}, {on={"c","n"}},
-    {on = {"w","u"}}, {on = {"w","i"}}, {on={"w","o"}}, {on={"w","h"}}, {on={"w","j"}}, {on={"w","k"}}, {on={"w","l"}}, {on={"w","n"}},
-}
-
-local t_signalKey = {'p','b','e', 't', 'a', 'o', 'i', 'n', 's', 'r', 'h',
-'l', 'd', 'c', 'u', 'm', 'f', 'g', 'w', 'v', 'k', 'j',
-'x','z', 'y','q'}
-
-local t_doubleKey = {
-    "au", "ai", "ao", "ah", "aj", "ak", "al", "an",
-    "su", "si", "so", "sh", "sj", "sk", "sl", "sn",
-    "du", "di", "do", "dh", "dj", "dk", "dl", "dn",
-    "fu", "fi", "fo", "fh", "fj", "fk", "fl", "fn",
-	"gu", "gi", "go", "gh", "gj", "gk", "gl", "gn",
-    "eu", "ei", "eo", "eh", "ej", "ek", "el", "en",
-    "ru", "ri", "ro", "rh", "rj", "rk", "rl", "rn",
-    "cu", "ci", "co", "ch", "cj", "ck", "cl", "cn",
-    "wu", "wi", "wo", "wh", "wj", "wk", "wl", "wn",
-}
 
 if args[1] == "sync-init" then
-	B_isJumpMode = false
-	B_isDoubleKey = false
-	B_isWinOS = false
-	S_modeType = ""
-	I_item_num = 0
+	IsInJumpMode = false
+	UseDoubleKey = false
+	IsInWinOS = false
+	KeyJumpType = ""
+	ItemNumInCurrentWindow = 0
 end
 
 -- util function
@@ -87,11 +94,11 @@ local function isWinOS()
 end
 
 local function setKeyMode()
-	I_item_num = getCurrenAreaItemNum()
-	if I_item_num > 26 then
-		B_isDoubleKey = true
+	ItemNumInCurrentWindow = getCurrenAreaItemNum()
+	if ItemNumInCurrentWindow > 26 then
+		UseDoubleKey = true
 	else
-		B_isDoubleKey = false
+		UseDoubleKey = false
 	end
 end
 
@@ -102,7 +109,7 @@ end
 
 local function getFileNumFromPath(path)
 	local cmd = "ls "
-	if B_isWinOS then
+	if IsInWinOS then
 		cmd = "dir "
 	end
 
@@ -118,14 +125,14 @@ end
 -- overwirte system function
 if Folder then
 	function Folder:icon(file)
-		if B_isJumpMode then
+		if IsInJumpMode then
 			local position = getFilePosition(file)
 			if position == 0 then
 				return ui.Span(" " .. file:icon().text .. " ")
-			elseif B_isDoubleKey then
-				return ui.Span(" " .. file:icon().text .. " "..t_doubleKey[position].. " ")
+			elseif UseDoubleKey then
+				return ui.Span(" " .. file:icon().text .. " "..DOUBLE_KEYS[position].. " ")
 			else
-				return ui.Span(" " .. file:icon().text .. " "..t_signalKey[position].. " ")
+				return ui.Span(" " .. file:icon().text .. " "..SINGLE_KEYS[position].. " ")
 			end
 		else
 			return ui.Span(" " .. file:icon().text .. " ")
@@ -136,8 +143,10 @@ end
 if Status then
 	function Status:mode()
 		local mode = "UNSET"
-		if B_isJumpMode then
-			mode = "KJ-" .. tostring(cx.active.mode):upper()
+		if IsInJumpMode and KeyJumpType == "normal" then
+			mode = "KJN-" .. tostring(cx.active.mode):upper()
+		elseif IsInJumpMode and KeyJumpType == "keep" then
+				mode = "KJK-" .. tostring(cx.active.mode):upper()
 		else
 			mode = tostring(cx.active.mode):upper() -- accessing the current context through cx
 		end
@@ -155,20 +164,20 @@ if Status then
 end
 
 local function init()
-	B_isJumpMode = true
-	S_modeType = args[2]
+	IsInJumpMode = true
+	KeyJumpType = args[2]
 	setKeyMode()
-	B_isWinOS =	isWinOS()
+	IsInWinOS =	isWinOS()
 	ya.render()
-	ya.manager_emit("plugin", { "keyjump", args = tostring("async-getinput").." "..tostring(B_isDoubleKey).." "..tostring(I_item_num) })
+	ya.manager_emit("plugin", { "keyjump", args = tostring("async-getinput").." "..tostring(UseDoubleKey).." "..tostring(ItemNumInCurrentWindow) })
 end
 
 local function getinput()
 	local target_candy
 	if args[2] == "false" then
-		target_candy = {table.unpack(t_signalKey_candy, 1, args[3])}
+		target_candy = {table.unpack(SIGNAL_CANDS, 1, args[3])}
 	else
-		target_candy = {table.unpack(t_doubleKey_candy, 1, args[3])}
+		target_candy = {table.unpack(DOUBLE_CANDS, 1, args[3])}
 	end
 	local key = ya.which {
 		cands = target_candy,
@@ -183,7 +192,7 @@ end
 
 local function normalAction()
 	local cursor_position = getCursorPosition()
-	B_isJumpMode = false
+	IsInJumpMode = false
 	ya.render()
 	ya.manager_emit("arrow", {tostring(tonumber(args[1]) - cursor_position ) })	
 end
@@ -195,28 +204,28 @@ local function keepAction()
 	if target_file and target_file.cha.is_dir then
 		ya.manager_emit("enter",{})	 -- TODO: need to block
 
-		local I_item_num = getFileNumFromPath(tostring(target_file.url))
-		if I_item_num == 0 then
-			B_isJumpMode = false
+		local ItemNumInCurrentWindow = getFileNumFromPath(tostring(target_file.url))
+		if ItemNumInCurrentWindow == 0 then
+			IsInJumpMode = false
 			ya.render()	
 			return		
 		end
 
-		if I_item_num > 26 then
-			B_isDoubleKey = true
+		if ItemNumInCurrentWindow > 26 then
+			UseDoubleKey = true
 		else
-			B_isDoubleKey = false
+			UseDoubleKey = false
 		end
 
-		ya.manager_emit("plugin", { "keyjump", args = tostring("async-getinput").." "..tostring(B_isDoubleKey).." "..tostring(I_item_num) })
+		ya.manager_emit("plugin", { "keyjump", args = tostring("async-getinput").." "..tostring(UseDoubleKey).." "..tostring(ItemNumInCurrentWindow) })
 	else
-		B_isJumpMode = false
+		IsInJumpMode = false
 		ya.render()		
 	end
 end
 
 local function nilkeyAction()
-	B_isJumpMode = false
+	IsInJumpMode = false
 	ya.render()			
 end
 
@@ -228,9 +237,9 @@ function M:entry()
 	elseif args[1] == "sync-nilkey" then --sync context
 		nilkeyAction()
 	else --sync context
-		if S_modeType == "normal" then
+		if KeyJumpType == "normal" then
 			normalAction()
-		elseif S_modeType == "keep" then
+		elseif KeyJumpType == "keep" then
 			keepAction()
 		else
 			ya.err("unknow action")

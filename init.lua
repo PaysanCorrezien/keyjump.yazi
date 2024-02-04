fb10b0b77704d67ed48ba30a8b96962d54a64f36
sxyazi <sxyazi@gmail.com>
2024-02-04 08:43:20 +0800

    refactor: simplify the code

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
-local M = {}
-
 local args = { ... }
 
+-- stylua: ignore
 local SINGLE_KEYS = {
 	"p", "b", "e", "t", "a", "o", "i", "n", "s", "r", "h", "l", "d", "c",
 	"u", "m", "f", "g", "w", "v", "k", "j", "x", "z", "y", "q"
@@ -52,194 +51,69 @@ local DOUBLE_CANDS = {
 	{ on = { "w", "k" } }, { on = { "w", "l" } }, { on = { "w", "n" } },
 }
 
-local function getFilePosition(file)
+-- TODO: refactor this
+local function rel_position(file)
 	for i, f in ipairs(Folder:by_kind(Folder.CURRENT).window) do
 		if f == file then
 			return i
 		end
 	end
-    return 0
 end
 
-local function getCurrenAreaItemNum()
-	return #(Folder:by_kind(Folder.CURRENT).window)
-end
-
-local function getCursorPosition()
-    return Folder:by_kind(Folder.CURRENT).cursor - Folder:by_kind(Folder.CURRENT).offset
-end
-
-local function isWinOS()
-	local home_path = os.getenv("HOME")
-	if home_path == nil then
-		return true
-	end
-	
-	local result = string.match(home_path,":")
-	if result ~= nil then
-		return true
-	end
-
-	return false
-end
-
-local function setKeyMode()
-	ItemNumInCurrentWindow = getCurrenAreaItemNum()
-	if ItemNumInCurrentWindow > 26 then
-		UseDoubleKey = true
-	else
-		UseDoubleKey = false
-	end
-end
-
-
-local function getFileFromPosition(pos)
-	return (Folder:by_kind(Folder.CURRENT).window)[pos]
-end
-
-local function getFileNumFromPath(path)
-	local cmd = "ls "
-	if IsInWinOS then
-		cmd = "dir "
-	end
-
-    local a = io.popen(cmd..path);
-    local f = {};
-    for l in a:lines() do
-        table.insert(f,l)
-    end
-    a:close()
-    return #f
-end
-
-local function init()
-	-- init global var
-	UseDoubleKey = false
-	ItemNumInCurrentWindow = 0
-	IsInJumpMode = true
-	KeyJumpType = args[2]
-	IsInWinOS =	isWinOS()
-
-	setKeyMode()
+local function toggle_ui(state)
 	ya.render()
-	ya.manager_emit("plugin", { "keyjump", args = tostring("async-getinput").." "..tostring(UseDoubleKey).." "..tostring(ItemNumInCurrentWindow) })
-end
-
-local function getinput()
-	local target_candy
-	if args[2] == "false" then
-		target_candy = {table.unpack(SIGNAL_CANDS, 1, args[3])}
-	else
-		target_candy = {table.unpack(DOUBLE_CANDS, 1, args[3])}
+	if state.icon or state.mode then
+		Folder.icon, Status.mode, state.icon, state.mode = state.icon, state.mode, nil, nil
+		return
 	end
-	local key = ya.which {
-		cands = target_candy,
-		silent = true, 
-	}
-	if key ~= nil then
-		ya.manager_emit("plugin", { "keyjump", sync = "", args = tostring(key - 1) })
-	else
-		ya.manager_emit("plugin", { "keyjump", sync = "", args = tostring("sync-nilkey") })
-	end		
-end
 
-local function normalAction()
-	local cursor_position = getCursorPosition()
-	IsInJumpMode = false
-	ya.render()
-	ya.manager_emit("arrow", {tostring(tonumber(args[1]) - cursor_position ) })	
-end
-
-local function keepAction()
-	local cursor_position = getCursorPosition()
-	ya.manager_emit("arrow", {tostring(tonumber(args[1]) - cursor_position ) })	
-	local target_file = getFileFromPosition(tonumber(args[1])+1)
-	if target_file and target_file.cha.is_dir then
-		ya.manager_emit("enter",{})	 -- TODO: need to block
-
-		local ItemNumInCurrentWindow = getFileNumFromPath(tostring(target_file.url))
-		if ItemNumInCurrentWindow == 0 then
-			IsInJumpMode = false
-			ya.render()	
-			return		
-		end
-
-		if ItemNumInCurrentWindow > 26 then
-			UseDoubleKey = true
+	state.icon, state.mode = Folder.icon, Status.mode
+	Folder.icon = function(self, file)
+		local pos = rel_position(file)
+		if not pos then
+			return state.icon(self, file)
+		elseif state.num > 26 then
+			return ui.Span(DOUBLE_KEYS[pos] .. " " .. file:icon().text .. " ")
 		else
-			UseDoubleKey = false
-		end
-
-		ya.manager_emit("plugin", { "keyjump", args = tostring("async-getinput").." "..tostring(UseDoubleKey).." "..tostring(ItemNumInCurrentWindow) })
-	else
-		IsInJumpMode = false
-		ya.render()		
-	end
-end
-
-local function nilkeyAction()
-	IsInJumpMode = false
-	ya.render()			
-end
-
-function M:entry()
-	if args[1] == "sync-init" then --sync context
-		init()
-	elseif args[1] == "async-getinput" then --async context
-		getinput()
-	elseif args[1] == "sync-nilkey" then --sync context
-		nilkeyAction()
-	else --sync context
-		if KeyJumpType == "normal" then
-			normalAction()
-		elseif KeyJumpType == "keep" then
-			keepAction()
-		else
-			ya.err("unknow action")
+			return ui.Span(SINGLE_KEYS[pos] .. " " .. file:icon().text .. " ")
 		end
 	end
-end
-
--- overwirte system function
-if Folder then
-	function Folder:icon(file)
-		if IsInJumpMode then
-			local position = getFilePosition(file)
-			if position == 0 then
-				return ui.Span(" " .. file:icon().text .. " ")
-			elseif UseDoubleKey then
-				return ui.Span(" " .. file:icon().text .. " "..DOUBLE_KEYS[position].. " ")
-			else
-				return ui.Span(" " .. file:icon().text .. " "..SINGLE_KEYS[position].. " ")
-			end
-		else
-			return ui.Span(" " .. file:icon().text .. " ")
-		end
-	end
-end
-
-if Status then
-	function Status:mode()
-		local mode = "UNSET"
-		if IsInJumpMode and KeyJumpType == "normal" then
-			mode = "KJN-" .. tostring(cx.active.mode):upper()
-		elseif IsInJumpMode and KeyJumpType == "keep" then
-				mode = "KJK-" .. tostring(cx.active.mode):upper()
-		else
-			mode = tostring(cx.active.mode):upper() -- accessing the current context through cx
-		end
-
-		if mode == "UNSET" then
-			mode = "UN-SET"
-		end
-
+	Status.mode = function(self)
 		local style = self.style()
 		return ui.Line {
 			ui.Span(THEME.status.separator_open):fg(style.bg),
-			ui.Span(" " .. mode .. " "):style(style),
+			ui.Span(" KJ-" .. tostring(cx.active.mode):upper() .. " "):style(style),
 		}
 	end
 end
 
+local function next(sync, args) ya.manager_emit("plugin", { "keyjump", sync = sync, args = table.concat(args, " ") }) end
 
-return M
+return {
+	entry = function()
+		local action = args[1]
+		if not action then
+			state.num = #Folder:by_kind(Folder.CURRENT).window
+			toggle_ui(state)
+			return next(false, { "prompt", state.num })
+		end
+
+		if action == "prompt" then
+			local cands, num = nil, tonumber(args[2])
+			if num > 26 then
+				cands = { table.unpack(DOUBLE_CANDS, 1, num) }
+			else
+				cands = { table.unpack(SIGNAL_CANDS, 1, num) }
+			end
+
+			local key = ya.which { cands = cands, silent = true }
+			return next(true, key and { "apply", key } or { "reset" })
+		end
+
+		toggle_ui(state)
+		if action == "apply" then
+			local pos = Folder:by_kind(Folder.CURRENT).cursor - Folder:by_kind(Folder.CURRENT).offset
+			ya.manager_emit("arrow", { tostring(args[2] - pos - 1) })
+		end
+	end,
+}
